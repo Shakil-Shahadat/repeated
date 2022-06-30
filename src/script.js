503306d2c3a8e647b0d07c88f788601edff314bd
Shakil Shahadat <shakil.regi@gmail.com>
2022-06-30 06:01:49 +0600

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-// A function to show buttons on mouse enter into a card
+// Show buttons when mouse enter into a card
 function showButtons( catchMe )
 {
 	catchMe.querySelector( '.button-group' ).classList.remove( 'd-none' );
