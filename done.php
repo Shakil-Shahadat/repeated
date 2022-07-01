a77b98cab222948d9aa32ef24335013d6b6e805c
Shakil Shahadat <shakil.regi@gmail.com>
2022-07-02 03:48:53 +0600

    Create done.php

---
+++ done.php
@@ -0,0 +1,12 @@
+<?php
+$db = new SQLite3( 'data.db' );
+
+if ( $_POST[ 'task' ] != '' )
+{
+	$response = $db->exec( 'UPDATE tasks SET done=1 WHERE task="' . $_POST[ 'task' ] . '"' );
+	echo 'Success!';
+}
+else
+{
+	echo 'Fail!';
+}
