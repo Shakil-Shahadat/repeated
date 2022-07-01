9f070df011823cc07e8aca89b7be70900928058b
Shakil Shahadat <shakil.regi@gmail.com>
2022-07-02 04:12:40 +0600

    Update add.php

--- add.php
+++ add.php
@@ -5,7 +5,7 @@ $db = new SQLite3( 'data.db' );
 
 if ( $_POST[ 'task' ] != '' && $_POST[ 'type' ] != '' )
 {
-	$response = $db->exec( 'INSERT INTO tasks VALUES ( "' . $_POST[ 'task' ] . '", "' . $_POST[ 'type' ] . '", "" )' );
+	$response = $db->exec( 'INSERT INTO tasks VALUES ( "' . $_POST[ 'task' ] . '", "' . $_POST[ 'type' ] . '", 0 )' );
 	echo 'Success!';
 }
 else
