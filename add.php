1b97e2e3f02eefd586d8469286bcf15dbc6bda66
Shakil Shahadat <shakil.regi@gmail.com>
2022-07-01 15:28:29 +0600

    Update add.php

--- add.php
+++ add.php
@@ -3,6 +3,12 @@ header( 'Access-Control-Allow-Origin: *' );
 
 $db = new SQLite3( 'data.db' );
 
-$response = $db->exec( 'INSERT INTO tasks VALUES ( "' . $_POST[ 'task' ] . '", "' . $_POST[ 'type' ] . '", "" )' );
-
-echo 'Success!';
+if ( $_POST[ 'task' ] != '' && $_POST[ 'type' ] != '' )
+{
+	$response = $db->exec( 'INSERT INTO tasks VALUES ( "' . $_POST[ 'task' ] . '", "' . $_POST[ 'type' ] . '", "" )' );
+	echo 'Success!';
+}
+else
+{
+	echo 'Fail!';
+}
