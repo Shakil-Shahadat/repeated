6567752a507f4280a621d1b76fa8f9cd9ea05f59
Shakil Shahadat <shakil.regi@gmail.com>
2022-07-02 04:10:41 +0600

    Update del.php

--- del.php
+++ del.php
@@ -4,9 +4,13 @@ $db = new SQLite3( 'data.db' );
 if ( $_POST[ 'task' ] != '' )
 {
 	$response = $db->exec( 'DELETE FROM tasks WHERE task="' . $_POST[ 'task' ] . '"' );
-	echo 'Success!';
-}
-else
-{
-	echo 'Fail!';
+
+	if ( $response )
+	{
+		echo 'Success!';
+	}
+	else
+	{
+		echo 'Fail!';
+	}
 }
