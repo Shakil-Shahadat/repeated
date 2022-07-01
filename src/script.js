3fc9cd5795b4ba3721fb448e3d14a0be688877cf
Shakil Shahadat <shakil.regi@gmail.com>
2022-07-01 17:24:40 +0600

    Update script.js

--- src/script.js
+++ src/script.js
@@ -41,3 +41,26 @@ function addTask()
 	})
 	.catch( error => console.error( 'Error:', error ) );
 }
+
+// Delete a task
+function delTask( target )
+{
+	fetch( 'del.php',
+	{
+		method: 'POST',
+		headers: { 'Content-Type' : 'application/x-www-form-urlencoded' },
+		body: 'del=' + encodeURIComponent( target.parentNode.parentNode.querySelector( '.card-title' ).innerText )
+	})
+	.then( response => response.text() )
+	.then( data => {
+		if ( data === 'Success!' )
+		{
+			location.reload();
+		}
+		else
+		{
+			alert( 'Last query failed.' );
+		}
+	})
+	.catch( error => console.error( 'Error:', error ) );
+}
